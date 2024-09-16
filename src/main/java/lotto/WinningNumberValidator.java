f049f85461cf83ca65f0b3c66f6aca3eeb5dd1fc
EunSeop <doosan1112@naver.com>
2024-09-16 16:49:49 +0900

    [Feat]당첨번호 입력

--- src/main/java/lotto/WinningNumberValidator.java
+++ src/main/java/lotto/WinningNumberValidator.java
@@ -1,2 +1,33 @@
-package lotto;public class WinningNumberValidator {
+package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WinningNumberValidator {
+
+    public static List<Integer> parseAndValidateWinningNumbers(String input) {
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        if (numbers.size() != 6 || !numbers.stream().allMatch(WinningNumberValidator::isValidNumber)) {
+            throw new IllegalArgumentException("당첨 번호 오류");
+        }
+
+        return numbers;
+    }
+
+    public static int validateBonusNumber(String input, List<Integer> winningNumbers) {
+        int number = Integer.parseInt(input);
+        if (!isValidNumber(number) || winningNumbers.contains(number)) {
+            throw new IllegalArgumentException("보너스 번호 오류");
+        }
+        return number;
+    }
+
+    private static boolean isValidNumber(int number) {
+        return number >= 1 && number <= 45;
+    }
 }
