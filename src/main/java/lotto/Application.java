d01069c66de489e6f9e5540f4c76b7d2951ba392
EunSeop <doosan1112@naver.com>
2024-09-17 21:04:59 +0900

    [Feat]main  함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,10 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BuyingLotto buyingLotto = new BuyingLotto();
+        LottoCreator lottoCreator = new LottoCreator(buyingLotto.getLottoCount());
+        lottoCreator.printLottoList();
+        LottoResult lottoResult = new LottoResult(lottoCreator.getLottoList());
+        lottoResult.printResult();
     }
 }
